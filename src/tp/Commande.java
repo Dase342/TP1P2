31b2090e0b20aa772950f1411fbdfecbba43a73f
david to <davidt_888@hotmail.com>
2019-02-06 09:54:23 -0500

    Finit class Commande

--- src/tp/Commande.java
+++ src/tp/Commande.java
@@ -1,5 +1,30 @@
 package tp;
 
 public class Commande {
-
+	private Client client;
+	private Plat plat;
+	
+	public Commande (Client clientCommande, Plat platCommande){
+		client = clientCommande;
+		plat = platCommande;
+	}
+	
+	public Client getClient(){
+		return client;
+	}
+	public Plat getPlat(){
+		return plat;
+	}
+	public void setClient(Client clientCommande){
+		client = clientCommande;
+	}
+	
+	public void setPlat(Plat platCommande){
+		plat = platCommande;
+	}
+	
+	public double getTotale(){
+		
+		return plat.getPrixPlat()*plat.getQte();
+	}
 }
