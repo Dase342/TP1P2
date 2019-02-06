3e236772d92e724f81d10297156adf951e03300a
david to <davidt_888@hotmail.com>
2019-02-06 09:35:46 -0500

    Finit class Plat

--- src/tp/Plat.java
+++ src/tp/Plat.java
@@ -1,5 +1,29 @@
 package tp;
 
 public class Plat {
-
+	private String nomPlat;
+	private double prixPlat;
+	
+	public Plat(String plat, double prix){
+		nomPlat = plat;
+		prixPlat = prix;
+		
+	}
+	
+	public String getNomPlat(){
+		return nomPlat;
+		
+	}
+	
+	public double getPrixPlat (){
+		return prixPlat;
+	}
+	
+	public void setNomPlat(String plat){
+		nomPlat = plat;
+	}
+	
+	public void setPrixPlat (double prix) {
+		prixPlat = prix;
+	}
 }
