4a151477c298a44c0aba9a0d1b5d7fc7076437e0
david to <davidt_888@hotmail.com>
2019-02-06 10:02:15 -0500

    Ajout de methode et variable total et override toString

--- src/tp/Client.java
+++ src/tp/Client.java
@@ -2,10 +2,12 @@ package tp;
 
 public class Client {
 	private String nom;
+	private double total = 0;
 	
 	public Client (String nomClient){
 		nom = nomClient;
 	}
+	
 	
 	public String getNom(){
 		return nom;
@@ -14,5 +16,20 @@ public class Client {
 	public void setNom(String nomClient){
 		nom = nomClient;
 	}
+	
+	public void setTotal(double montant){
+		total = montant;
+	}
+	
+	public void addTotal(double montant){
+		total += montant;
+	}
+	
+	@Override
+	
+	public String toString(){
+		return nom + " " + total + "$";
+	}
+	
 
 }
