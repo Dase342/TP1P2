a537d590753aa0ed595760d431acec9852c8751c
david to <davidt_888@hotmail.com>
2019-02-06 10:08:45 -0500

    base de la classe Main finit

--- src/tp/Main.java
+++ src/tp/Main.java
@@ -1,8 +1,17 @@
 package tp;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		List<Plat>  listPlat;
+		
+		List<Client> listClient;
+		
+		List<Commande> listCommande;
+		
 		
 
 	}
