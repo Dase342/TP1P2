30cbc846b5ade2bcc933154714cad47e231a39d1
david to <davidt_888@hotmail.com>
2019-02-06 09:26:14 -0500

    Finit class Client

--- src/tp/Client.java
+++ src/tp/Client.java
@@ -1,5 +1,18 @@
 package tp;
 
 public class Client {
+	private String nom;
+	
+	public Client (String nomClient){
+		nom = nomClient;
+	}
+	
+	public String getNom(){
+		return nom;
+	}
+	
+	public void setNom(String nomClient){
+		nom = nomClient;
+	}
 
 }
